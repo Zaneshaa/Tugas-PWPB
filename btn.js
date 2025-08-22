8fefc0aa3d3fe9d6be7ad19e629aa88f87a50093
Zaneshaa <ahfauz1911@gmail.com>
2025-08-22 17:50:08 +0700

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,3 @@
+function bakekok() {
+  alert('Nama Lengkap: Ahmad Fauzan\nHobi: Maen Bola\nUmur: 17\nNama Sekolah: MK\nTTL: SYDNEY, 19 NOVEMBER 2007');
+}
---
+++ divid.js
@@ -0,0 +1,4 @@
+const divId = document.getElementById('tujuan');
+
+
+divId.innerHTML = "<p>Keinginan: Ke New Trafford<br>Hobi: Maen bola<br>Tujuan Hidup: Ngoleksi jersey King Emyu 👑 </p>"
